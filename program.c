ec6934f4fb730a51802c06b9b5f8090f0398e693
HankHill <hhc3h8@gmail.com>
2023-04-15 07:48:55 -0500

    Bored and don't know what to make.

--- program.c
+++ program.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
 
 int main() {
 	char hello[] = "Hello World";
+	int* helloPtr = &hello;
 
-	printf("%s", hello);
+	printf("%d\n", *helloPtr);
 	return 0;
 }
